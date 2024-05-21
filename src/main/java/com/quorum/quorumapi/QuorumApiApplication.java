6350367c3f91489dc37fdc74573312f372866dca
Kevin <kevin.macias.a@gmail.com>
2024-05-21 16:27:34 -0700

    Initial commit. Spring boot boiler plate

---
+++ src/main/java/com/quorum/quorumapi/QuorumApiApplication.java
@@ -0,0 +1,13 @@
+package com.quorum.quorumapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuorumApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuorumApiApplication.class, args);
+	}
+
+}
